fc78b8576dbbadb1cd51a111d9cbb83f2dd56cd2
hlushak <75501308+MarynaHl@users.noreply.github.com>
2022-10-17 12:37:06 +0100

    delete styles

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Section = ({title , children }) => (
     <>
-        <h2 class="Section_title__SLHvV" >{title}</h2>
+        <h2>{title}</h2>
         {children}
     </>
 )
